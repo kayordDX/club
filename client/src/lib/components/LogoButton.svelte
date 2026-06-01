343cd7d8da8b76d1471ce2ceeb503e59badab458
Jaco Kok <kokjaco2@gmail.com>
2026-06-01 20:03:44 +0200

    Renaming

--- client/src/lib/components/LogoButton.svelte
+++ client/src/lib/components/LogoButton.svelte
@@ -5,5 +5,5 @@
 
 <button class="flex items-center gap-1" onclick={() => goto(resolve("/"))}>
 	<img src="/favicon.svg" alt="logo" class="h-10" />
-	<span class="hidden text-lg font-bold md:block">Online</span>
+	<span class="hidden text-lg font-bold md:block">Club</span>
 </button>
