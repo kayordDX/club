fa8c2779dd3a968a3808e19da489e560d97ed7d2
Jaco Kok <kokjaco2@gmail.com>
2026-07-15 12:45:37 +0200

    We now linking payment to facility.

--- api/src/club/Entities/PaymentProviderConfig.cs
+++ api/src/club/Entities/PaymentProviderConfig.cs
@@ -3,6 +3,8 @@ namespace Club.Entities;
 public class PaymentProviderConfig : AuditableEntity
 {
     public int Id { get; set; }
+    public int FacilityId { get; set; }
+    public Facility Facility { get; set; } = default!;
     public required string ProviderKey { get; set; }
     public byte[] Iv { get; set; } = [];
     public required string EncryptedSettings { get; set; }
