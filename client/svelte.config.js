89775708a362e64e59c2e89c5a7676eee389a826
Jaco Kok <kokjaco2@gmail.com>
2026-08-06 11:06:07 +0200

    Update demo page to use outlet remote function with async boundary

--- client/svelte.config.js
+++ client/svelte.config.js
@@ -16,6 +16,11 @@ const config = {
 			remoteFunctions: true,
 		},
 	},
+	compilerOptions: {
+		experimental: {
+			async: true,
+		},
+	},
 	vitePlugin: {
 		inspector: {
 			showToggleButton: "never",
