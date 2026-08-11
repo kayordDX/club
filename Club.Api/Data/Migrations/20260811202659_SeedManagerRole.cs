150513cfda8b9b39df2457b1a4645932d7fa2fee
Jaco Kok <kokjaco2@gmail.com>
2026-08-11 22:35:20 +0200

    Seed manager role

---
+++ Club.Api/Data/Migrations/20260811202659_SeedManagerRole.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Club.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedManagerRole : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                """
+                INSERT INTO role (id, name, normalized_name, concurrency_stamp)
+                VALUES (gen_random_uuid(), 'manager', 'MANAGER', NULL)
+                ON CONFLICT (normalized_name) DO NOTHING;
+                """
+            );
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(
+                """
+                DELETE FROM role
+                WHERE name = 'manager';
+                """
+            );
+        }
+    }
+}
